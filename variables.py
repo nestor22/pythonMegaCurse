9400b6b792eda048f6e82849e574f9e093c24b9a
salem rios <nestor.ge222@gmail.com>
2019-07-26 09:16:34 -0600

    dia 1 python mega course

---
+++ variables.py
@@ -0,0 +1,38 @@
+print("hello")
+#this is a estring
+algo = input("ingresa un texto ")
+
+print(algo)
+
+
+age = int(input("enter your age:"))
+new_age = age +50
+print(new_agre)
+
+
+#strings
+c = "Here"
+c.replace("e", "i")
+print(c)
+c.replace("i", "e",  1)
+
+#substrig
+c = "hi there!"
+print(c[0])
+print(c[-1])
+print(c[:2])
+
+#listas
+c = ["hi", 1, "there"]
+print(c)
+c.append(3)
+print(c)
+c.remove
+#tubpla
+t = ("hello", 3,4.5, "algo")
+#dicctionarios
+d = {"name":"Nestor", "lastname":"Gonzalez", "cities":("Guatemala", "VilaNueva", "San Miguel")}
+print d["name"]
+print d["cities"]
+print d["cities"][2]
+
